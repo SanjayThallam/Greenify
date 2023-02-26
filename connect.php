23a52eec05695d1d94e405f93d896f12fa7fb944
SanjayThallam <60016462+SanjayThallam@users.noreply.github.com>
2023-02-26 01:52:32 -0500

    Uploaded Profhacks 2023 Code

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$user = 'root';
+$pass = '';
+$db = 'profhacks';
+
+$db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect");
+
+
+echo"I hate my life";
+?>
---
+++ dropoff.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<meta charset="UTF-8">
+<title>Greenify</title>
+<style>
+    body{
+        font-family: Arail, sans-serif;
+    }
+    /* Formatting search box */
+    .search-box{
+        width: 300px;
+        position: relative;
+        display: inline-block;
+        font-size: 14px;
+    }
+    .search-box input[type="text"]{
+        height: 32px;
+        padding: 5px 10px;
+        border: 1px solid #CCCCCC;
+        font-size: 14px;
+    }
+    .result{
+        position: absolute;        
+        z-index: 999;
+        top: 100%;
+        left: 0;
+    }
+    .search-box input[type="text"], .result{
+        width: 100%;
+        box-sizing: border-box;
+    }
+    /* Formatting result items */
+    .result p{
+        margin: 0;
+        padding: 7px 10px;
+        border: 1px solid #CCCCCC;
+        border-top: none;
+        cursor: pointer;
+    }
+    .result p:hover{
+        background: #f2f2f2;
+    }
+</style>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script>
+$(document).ready(function(){
+    $('.search-box input[type="text"]').on("keyup input", function(){
+        /* Get input value on change */
+        var inputVal = $(this).val();
+        var resultDropdown = $(this).siblings(".result");
+        if(inputVal.length){
+            $.get("backend-search.php", {term: inputVal}).done(function(data){
+                // Display the returned data in browser
+                resultDropdown.html(data);
+            });
+        } else{
+            resultDropdown.empty();
+        }
+    });
+    
+    // Set search input value on click of result item
+    $(document).on("click", ".result p", function(){
+        $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
+        $(this).parent(".result").empty();
+    });
+});
+</script>
+</head>
+<body>
+<div class="jumbotron text-center">
+        <h1>Greenify</h1>
+        <p>Your one stop shop for New Jersey's Trash and Recycling Info</p>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <ul class="nav navbar-nav">
+            <li class="active" style="color: white"><a href="main.html">Home</a></li>
+            <li><a href="trashtype.html">Recycle and Trash Types</a></li>
+            <li><a href="dropoff.php">Find a Drop Off Location</a></li>
+            <li><a href="maps.html">Google Maps Integration</a></li>
+          </ul>
+        </div>
+      </nav>
+    <h2> Use this page to find facilities that support the trash/recycling you are throwing</h2> 
+    <h3> Recycling: </h3>
+    <div class="search-box ">
+        <input type="text" autocomplete="off" placeholder="Search Recycling Types" />
+        <div class="result"></div>
+    </div>
+
+    
+</body>
+</html>
